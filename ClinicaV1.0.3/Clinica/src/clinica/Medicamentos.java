0609ec02a658a90adc6d036d5f8daae101d8dab5
daviserraalonso <daviserraalonso@gmail.com>
2018-04-17 17:38:14 +0200

    Arreglo de clase Medicamentos
    He arreglado la clase medicamentos

--- ClinicaV1.0.3/Clinica/src/clinica/Medicamentos.java
+++ ClinicaV1.0.3/Clinica/src/clinica/Medicamentos.java
@@ -14,24 +14,27 @@ import java.util.ArrayList;
  * @author Diurno
  */
 public class Medicamentos {
-    ArrayList<String>medicamentos=new ArrayList<String>();
+    ArrayList<String> medicamentos;
     
-    
+    public Medicamentos(){
+        medicamentos = new ArrayList<String>();
+    }
 
     public String getMedicamentos(int n) {
         return medicamentos.get(n);
     }
 
     public void insertaMedicamentos(String nomMed) {
-        
         medicamentos.add(nomMed);
-      
-      
     }
+    
     public String toString(){
     
-    String cadena="Medicamentos: ";
-    for(int i=0;i<medicamentos.size();i++){
-    cadena+=medicamentos.get(i)+" ";}
-        return cadena;}
+    String cadena = "Medicamentos: ";
+        for(int i=0;i<medicamentos.size();i++){
+            cadena+=medicamentos.get(i) + " ";
+
+        }
+    return cadena;
+    }
 }
