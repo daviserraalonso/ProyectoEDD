9ef35a402b8dc3a2aa3bb8c08a7aba5b1ee6c33f
daviserraalonso <daviserraalonso@gmail.com>
2018-06-13 23:32:38 +0200

    cambios
    cambios

--- clinicaprivada.sql
+++ clinicaprivada.sql
@@ -1,30 +1,4 @@
--- phpMyAdmin SQL Dump
--- version 4.6.5.2
--- https://www.phpmyadmin.net/
---
--- Servidor: 127.0.0.1
--- Tiempo de generación: 04-05-2018 a las 00:01:02
--- Versión del servidor: 10.1.21-MariaDB
--- Versión de PHP: 5.6.30
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Base de datos: `clinicaprivada`
---
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `consulta`
---
+use qzx072;
 
 CREATE TABLE `consulta` (
   `numConsulta` int(10) NOT NULL
@@ -223,6 +197,3 @@ ALTER TABLE `receta`
   ADD CONSTRAINT `recetaMedico` FOREIGN KEY (`idMedico`) REFERENCES `medico` (`idM`) ON DELETE SET NULL ON UPDATE SET NULL,
   ADD CONSTRAINT `recetaPaciente` FOREIGN KEY (`Paciente`) REFERENCES `paciente` (`NUS`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
